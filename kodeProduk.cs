e63c824d077755c707e431506fde1effceca60c3
JidanAzizi <idanaf3@gmail.com>
2025-03-11 06:55:40 +0700

    menambahkan class kodeProduk

---
+++ kodeProduk.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurnal4
+{
+    class kodeProduk
+    {
+        public string nama;
+        private String kode;
+
+        public kodeProduk(string nama, String kode)
+        {
+            this.nama = nama;
+            this.kode = kode;
+        }
+
+        public string getKodeProduk() {
+            return kode;
+        }
+    }
+}
